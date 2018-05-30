db26dbde65a0639cf4ec8aaeddec46def7bcefec
Pieter Venter <pieter@ebit.co.za>
2018-05-30 10:58:48 +0200

    initial module

---
+++ src/index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const { renderPlaygroundPage } = use('graphql-playground-html')
+const { playgroundVersion } = require('../package.json')
+
+const graphQLPlayground = function graphQLPlayground (options) {
+  const graphqlPlaygroundHandler = ctx => {
+    const middlewareOptions = {
+      ...options,
+      version: playgroundVersion
+    }
+    const { request, response } = ctx
+    const playground = renderPlaygroundPage(middlewareOptions)
+    response.type('text/html').send(graphiqlString)
+  }
+  return graphqlPlaygroundHandler
+}
+
+module.exports = graphQLPlayground
